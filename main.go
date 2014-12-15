6cb25f2f6c475a99d38d7e8f4cccd67e083257dd
Falaleev Maxim <max@studio107.ru>
2014-12-15 11:04:29 +0300

    up

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ const (
 
 type Command struct {
 	Plugin string
+	Action string
 	Args   map[string]interface{}
 }
 
@@ -63,6 +64,53 @@ func GetMongoMsg() string {
 	args["username"] = "mimictl_user"
 	args["password"] = "123456"
 	cmd := &Command{Plugin: "mongo", Args: args}
+
+	msg, err := json.Marshal(cmd)
+	if err != nil {
+		panic(err)
+	}
+	return string(msg)
+}
+
+func GetOpenvzMsg() string {
+	args := make(map[string]interface{})
+
+	cpuunits := "600"
+	cpulimit := "25"
+	memorylimit := 256
+	disklimit := 2048
+
+	args["cpuunits"] = cpuunits
+	args["cpulimit"] = cpulimit
+
+	args["ram"] = fmt.Sprintf("%sM", memorylimit)
+	args["swap"] = fmt.Sprintf("%sM", memorylimit*2)
+
+	args["vmguarpages"] = fmt.Sprintf("%sM", memorylimit)
+	args["oomguarpages"] = fmt.Sprintf("%sM", memorylimit)
+	args["privvmpages"] = fmt.Sprintf("%sM:%sM", memorylimit, memorylimit*2)
+
+	args["diskspace"] = fmt.Sprintf("%sM:%sM", disklimit, disklimit+200)
+	args["diskinodes"] = fmt.Sprintf("%s:%s", 300000*(disklimit/1024), 320000*(disklimit/1024))
+
+	// args["ioprio"] = fmt.Sprintf("%s", 4)
+
+	// максимальное количество процессов контейнере (защитит от форкбомбы)
+	// args["numproc"] = "1024:1024"
+	// максимальное количество TCP-сокетов
+	// args["numtcpsock"] = "1024:1024"
+	// максимальное количество не TCP-сокетов
+	// args["numothersock"] = "1024:1024"
+
+	// ограничения на размер буфера отправки TCP
+	// args["tcpsndbuf"] = "1m:2m"
+	// ограничения на размер буфера приема TCP
+	// args["tcprcvbuf"] = "1m:2m"
+
+	// максимальное количество открытых файлов
+	args["numfile"] = "4096"
+
+	cmd := &Command{Plugin: "openvz", Action: "Create", Args: args}
 
 	msg, err := json.Marshal(cmd)
 	if err != nil {
@@ -142,7 +190,7 @@ func SendCommand(server string, timeout time.Duration, retries int, msg string) 
 }
 
 func main() {
-	msg := GetMongoMsg()
+	msg := GetOpenvzMsg()
 	err := SendCommand(SERVER_ENDPOINT, REQUEST_TIMEOUT, REQUEST_RETRIES, msg)
 	if err != nil {
 		log.Printf("Fail")
